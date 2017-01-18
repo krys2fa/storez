2c5dc1114835d56e71a43b9d14f5e14b2b7e1e39
Christopher Amanor <kriskit7@gmail.com>
2017-01-18 09:54:35 +0000

    added more routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :stores
+  resources :agents
+  resources :branches
+  resources :products
+  resources :sales
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
